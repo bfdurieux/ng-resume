667d4664ccf6598b43eb0164b488d03a86671fc2
Bruno Durieux <bfdurieux@gmail.com>
2024-06-26 00:03:13 -0300

    update side nav

--- src/app/components/icon/icon.component.ts
+++ src/app/components/icon/icon.component.ts
@@ -1,6 +1,6 @@
 import {Component, ElementRef, Input, Renderer2} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {catchError, of, tap} from "rxjs";
+import {catchError, of, take, tap} from "rxjs";
 
 @Component({
   selector: 'app-icon',
@@ -11,25 +11,26 @@ import {catchError, of, tap} from "rxjs";
 })
 export class IconComponent {
   @Input() set icon (value: string) {
-    console.log(value)
-    this.source = 'assets/icons/github.svg';
     this.loadIcon(value);
   }
-  @Input() iconSize: string = '24px';
   @Input() iconColor: string = 'black';
   @Input() isClickable: boolean = false;
+  @Input() id: string = '0';
   source: string = '';
 
   constructor(private http: HttpClient, private elementRef: ElementRef, private renderer: Renderer2) {}
 
   private loadIcon(icon: string): void {
     const iconPath = `assets/icons/${icon}.svg`;
+    const existingIcon = document.getElementById(this.iconId)
+
 
     this.http
       .get(iconPath, { responseType: 'text' })
       .pipe(
+        take(1),
         tap((svgText) => {
-          const svgElement = this.renderer.createElement('span');
+          const svgElement = this.renderer.createElement('div');
           svgElement.innerHTML = svgText;
           const icon = svgElement.querySelector('svg');
           this.applyIconStyles(icon);
@@ -41,11 +42,22 @@ export class IconComponent {
         })
       )
       .subscribe();
+
+    if (existingIcon != null)
+      existingIcon.remove();
+
   }
 
   private applyIconStyles(svgElement: HTMLElement): void {
-    this.renderer.setStyle(svgElement, 'width', this.iconSize);
-    this.renderer.setStyle(svgElement, 'height', this.iconSize);
+    this.renderer.setStyle(svgElement, 'width', '90%');
+    this.renderer.setStyle(svgElement, 'height', '90%');
     this.renderer.setStyle(svgElement, 'fill', this.iconColor);
+    this.renderer.setAttribute(svgElement, 'id', this.iconId);
+    if (this.isClickable)
+      this.renderer.setStyle(svgElement, 'cursor', 'pointer');
+  }
+
+  get iconId(): string {
+    return `icon-${this.icon}-${this.id}`;
   }
 }
--- src/app/components/side-nav/side-nav.component.ts
+++ src/app/components/side-nav/side-nav.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 import {IconComponent} from "../icon/icon.component";
+import {animate, AUTO_STYLE, state, style, transition, trigger} from "@angular/animations";
+import {NgClass} from "@angular/common";
+
+const DEFAULT_DURATION = 300;
 
 @Component({
   selector: 'app-side-nav',
   standalone: true,
   imports: [
-    IconComponent
+    IconComponent,
+    NgClass
+  ],
+  animations: [
+    trigger('collapse', [
+      state('false', style({ height: AUTO_STYLE, visibility: AUTO_STYLE })),
+      state('true', style({ height: '0', visibility: 'hidden' })),
+      transition('false => true', animate(DEFAULT_DURATION + 'ms ease-in')),
+      transition('true => false', animate(DEFAULT_DURATION + 'ms ease-out'))
+    ])
   ],
   templateUrl: './side-nav.component.html',
   styleUrl: './side-nav.component.scss'
