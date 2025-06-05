137d48b8fb9b5da32d54fa04911060c477895f43
Bruno Durieux <bfdurieux@gmail.com>
2025-06-05 20:14:26 -0300

    deploy assets 3

--- src/app/components/view-resume/view-resume.component.ts
+++ src/app/components/view-resume/view-resume.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-view-resume',
   standalone: true,
   imports: [
-    NgOptimizedImage
   ],
   templateUrl: './view-resume.component.html',
   styleUrl: './view-resume.component.scss'
